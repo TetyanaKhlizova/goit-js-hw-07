a62ffbd22ebf84652fd5d4b09aa88e051018eade
TetyanaKhlizova <90279592+TetyanaKhlizova@users.noreply.github.com>
2022-01-25 16:50:49 +0200

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// Реализация делегирования на div.gallery и получение url большого изображения.
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// 2.Реализация делегирования на div.gallery и получение url большого изображения.
+// 3.Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
 // Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
 // Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
 // Разметка элемента галереи
@@ -24,5 +24,27 @@ import { galleryItems } from './gallery-items.js';
 // ⚠️ Следующий функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
 
 // Добавь закрытие модального окна по нажатию клавиши Escape. Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно. У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+const containerGallery = document.querySelector('div.gallery');
+const htmlGallery = galleryItems.reduce((acc, { preview, original, description }) => {
+    acc += `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src='${preview}',
+       data-source = '${original}',
+       alt='${description}',
+        />
+    </a>
+     </div>`;
+    return acc;
+},'');
+containerGallery.insertAdjacentHTML('beforeend', htmlGallery);
 
-console.log(galleryItems);
+ containerGallery.addEventListener('click', onImgClick);
+ function onImgClick(event) {
+     if (event.target.nodeName !== 'IMG') {
+         return;
+    }
+     console.log(event.target);
+ }
+//  console.log(galleryItems);
